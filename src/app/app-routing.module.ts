373eec4ea3fb3704fc29e7f27e7bd7d67c907541
María Laura Fehr Toledo <mlfehr@ingenia.es>
2019-11-28 09:26:37 +0100

    ActLaura28-11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import {AuthorizatedGuard} from './authorizated.guard';
+import { ForgotpassComponent } from './forgotpass/forgotpass.component';
 
-
-//const routes: Routes = [];
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthorizatedGuard]},
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home'}
+  { path: '**', redirectTo: '/home'},
+  { path: 'forgot', component: ForgotpassComponent}
 ];
 
 export const Routing = RouterModule.forRoot(appRoutes);
 
-/*@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }*/
+
--- src/app/models/Users.ts
+++ src/app/models/Users.ts
@@ -1,12 +1,23 @@
 export class Users {
     id: number;
+    name: string;
+    surname: string;
     username: string;
     pass?: string;
+    email: string;
+    extraEmail: string;
+    idGroup: number;
 
 
-    constructor(id: number, username: string,  pass: string) {
+    constructor(id: number, name: string, surname: string, username: string,
+                pass: string, email: string, extraEmail: string, idGroup: number) {
       this.id = id;
       this.username = username;
       this.pass = pass;
+      this.name = name;
+      this.surname = surname;
+      this.email = email;
+      this.extraEmail = extraEmail;
+      this.idGroup = idGroup;
     }
 }
