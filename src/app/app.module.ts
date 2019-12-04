267cc35385b818a5d67ca0003dd413158be562f8
María Laura Fehr Toledo <mlfehr@ingenia.es>
2019-12-04 12:41:17 +0100

    ActualizacionLaura04-12Mañana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es'; /** PARA FECHAS DE LIBERAR Y SOLICITAR (LISTADO) */
+registerLocaleData(localeEs, 'es'); /** PARA FECHAS DE LIBERAR Y SOLICITAR (LISTADO) */
 import { Routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersService } from './services/users.service';
@@ -9,7 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatCardModule,
         MatDialogModule, MatTableModule, MatProgressSpinnerModule, MatToolbarModule, MatSidenavModule,
-        MatListModule, MatNativeDateModule, MatDividerModule  } from '@angular/material';
+        MatListModule, MatNativeDateModule, MatDividerModule } from '@angular/material';
 import { HomeComponent } from './menu-principal/home/home.component';
 import { AuthorizatedGuard} from './authorizated.guard';
 import { StorageService} from './services/storage.service';
@@ -22,6 +24,8 @@ import { PerfilComponent } from './menu-principal/perfil/perfil.component';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, SatDatepickerModule } from 'saturn-datepicker';
 import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DialogComponent } from './menu-principal/dialog/dialog.component';
+import { registerLocaleData } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -34,6 +38,7 @@ import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-mo
     ReportarComponent,
     PerfilComponent,
     MenuPrincipalComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -48,8 +53,12 @@ import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-mo
     MatChipsModule, MatNativeDateModule, MatDividerModule,
     SatDatepickerModule
   ],
-  
+  entryComponents: [
+    DialogComponent
+  ],
+
   providers: [AuthorizatedGuard, StorageService,
+    {provide: LOCALE_ID, useValue: 'es'}, /** PARA FECHAS DE LIBERAR Y SOLICITAR (LISTADO) */
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ],
--- src/app/menu-principal/menu-principal.component.ts
+++ src/app/menu-principal/menu-principal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../services/storage.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from './dialog/dialog.component';
 
 
 
@@ -10,13 +12,28 @@ import { StorageService } from '../services/storage.service';
 })
 export class MenuPrincipalComponent implements OnInit {
 
-  constructor(private storageService: StorageService) { }
+  constructor(private storageService: StorageService,
+              public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  public openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '50%',
+      data: '¿Quieres cerrar sesion?'
+    });
+    dialogRef.afterClosed().subscribe( result => {
+      if(result){
+        console.log('yes clicked');
+        this.storageService.logout();
+      }
+    });
+  }
+
+
   public logout(): void {
     this.storageService.logout();
-  }
+   }
 
 }
